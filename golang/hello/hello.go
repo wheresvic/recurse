dd5bd958f495abd5d6ff16aa860e6e8b0f84651a
Victor Parmar <vic@smalldata.tech>
2019-04-08 15:00:44 +0200

    F add map example

--- golang/hello/hello.go
+++ golang/hello/hello.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+var data = make(map[string]int)
+var mutex = &sync.Mutex{}
 
 func main() {
 	fmt.Println("Hello World")
+
+	UpdateMap("a", 1)
+	UpdateMap("b", 2)
+	UpdateMapBetter("c", 3)
+	UpdateMapBest("d", 4)
+}
+
+// UpdateMap ...
+func UpdateMap(key string, value int) int {
+	result := 0
+	mutex.Lock()
+	data[key] = value
+
+	for _, v := range data {
+		result += v
+	}
+	mutex.Unlock()
+	log.Printf("calculated result %d", result)
+	return result
+}
+
+// UpdateMapBetter ..
+func UpdateMapBetter(key string, value int) int {
+	result := 0
+	mutex.Lock()
+	defer mutex.Unlock()
+	data[key] = value
+
+	for _, v := range data {
+		result += v
+	}
+
+	log.Printf("calculated result %d", result)
+	return result
+}
+
+// UpdateMapBest ...
+func UpdateMapBest(key string, value int) int {
+	result := 0
+
+	func() {
+		mutex.Lock()
+		defer mutex.Unlock()
+		data[key] = value
+
+		for _, v := range data {
+			result += v
+		}
+	}()
+
+	log.Printf("calculated result %d", result)
+	return result
 }
